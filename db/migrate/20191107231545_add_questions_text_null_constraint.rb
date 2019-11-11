a8ad3178a80a967cfca9fde3224313a3d5106c4f
bulatkhamitov <bulat.khamitov@gmail.com>
2019-11-12 01:00:53 +0300

    Delete old migrations

--- db/migrate/20191107231545_add_questions_text_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class AddQuestionsTextNullConstraint < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null(:questions, :text, false)
-  end
-end
--- db/migrate/20191107231934_add_answers_correct_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class AddAnswersCorrectNullConstraint < ActiveRecord::Migration[6.0]
-  def change
-    change_column_null(:answers, :correct, false)
-  end
-end
