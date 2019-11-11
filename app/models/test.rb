02974d0e6cfffeb311a1ad2769d27460a5808904
bulatkhamitov <bulat.khamitov@gmail.com>
2019-11-11 04:33:27 +0300

    Add new methods for classes Test and User

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  class << self
+    def sort_by_category(category)
+      joins('JOIN categories ON tests.categories_id = categories_id').where(categories: { title: category }).order(title: :desc).pluck(:title)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def passed_by_level(level)
+    Test.joins('JOIN passedtests ON passedtests.tests_id = tests_id').where(tests: { level: level }, passedtests: { users_id: self.id })
+  end
 end
