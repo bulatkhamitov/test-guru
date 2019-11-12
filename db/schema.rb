2b2adfbbf3f3dff1d66890c724aab951f86b1e8c
bulatkhamitov <bulat.khamitov@gmail.com>
2019-11-12 04:22:20 +0300

    Update schema.rb and seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_10_230126) do
+ActiveRecord::Schema.define(version: 2019_11_12_000240) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
     t.string "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "questions_id"
-    t.index ["questions_id"], name: "index_answers_on_questions_id"
+    t.integer "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -27,21 +27,21 @@ ActiveRecord::Schema.define(version: 2019_11_10_230126) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "passedtests", force: :cascade do |t|
+  create_table "passed_tests", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "users_id"
-    t.integer "tests_id"
-    t.index ["tests_id"], name: "index_passedtests_on_tests_id"
-    t.index ["users_id"], name: "index_passedtests_on_users_id"
+    t.integer "user_id"
+    t.integer "test_id"
+    t.index ["test_id"], name: "index_passed_tests_on_test_id"
+    t.index ["user_id"], name: "index_passed_tests_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
     t.string "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "tests_id"
-    t.index ["tests_id"], name: "index_questions_on_tests_id"
+    t.integer "test_id"
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -49,23 +49,24 @@ ActiveRecord::Schema.define(version: 2019_11_10_230126) do
     t.integer "level", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "categories_id"
-    t.integer "users_id"
-    t.index ["categories_id"], name: "index_tests_on_categories_id"
-    t.index ["users_id"], name: "index_tests_on_users_id"
+    t.integer "user_id"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "answers", "questions", column: "questions_id"
-  add_foreign_key "passedtests", "tests", column: "tests_id"
-  add_foreign_key "passedtests", "users", column: "users_id"
-  add_foreign_key "questions", "tests", column: "tests_id"
-  add_foreign_key "tests", "categories", column: "categories_id"
-  add_foreign_key "tests", "users", column: "users_id"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "passed_tests", "tests"
+  add_foreign_key "passed_tests", "users"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
