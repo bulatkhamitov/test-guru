733e9432681607fcb96ffb45dec4f3ecaf770915
bulatkhamitov <bulat.khamitov@gmail.com>
2019-11-14 05:09:17 +0300

    Add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
+  has_many :questions
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
+
   def self.sort_by_category(category)
     joins('JOIN categories ON tests.category_id = categories.id').where(categories: { title: category }).order(title: :desc).pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :passed_tests
+  has_many :tests, through: :passed_tests
+
   def passed_by_level(level)
     Test.joins('JOIN passed_tests ON passed_tests.test_id = tests.id').where(passed_tests: { user_id: id }, level: level)
   end
