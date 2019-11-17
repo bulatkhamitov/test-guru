62387385e913c9a35d7deb0b771ca0e3231b3b3a
bulatkhamitov <bulat.khamitov@gmail.com>
2019-11-17 21:10:05 +0300

    Fix validation check

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,9 +6,7 @@ class Answer < ApplicationRecord
 
   scope :correct, -> { where(correct: true) }
 
-  private
-
   def validate_number_of_answers
-    errors.add(:question) unless self.class.where(question: question).count.between?(1, 4)
+    errors.add(:question) if question.answers.count >= 4
   end
 end
