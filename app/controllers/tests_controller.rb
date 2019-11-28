02b7326200ce122415f991efbc5e845a1685ae04
bulatkhamitov <bulat.khamitov@gmail.com>
2019-11-28 15:26:39 +0300

    Fix test controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -7,20 +7,15 @@ class TestsController < ApplicationController
     @tests = Test.all
   end
 
-  def show
-    @test = Test.find(params[:id])
-  end
+  def show; end
 
   def new
     @test = Test.new
   end
 
-  def edit
-    @test = Test.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @test = Test.find(params[:id])
     if @test.update(test_params)
       redirect_to @test
     else
@@ -38,7 +33,6 @@ class TestsController < ApplicationController
   end
 
   def destroy
-    @test = Test.find(params[:id])
     @test.destroy
     redirect_to tests_path
   end
