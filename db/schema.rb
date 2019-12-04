436d071576667dc300112c3acfa4280c64f96510
bulatkhamitov <bulat.khamitov@gmail.com>
2019-12-05 00:26:44 +0300

    Update DB schema in accordance to 'TestPassage' model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_000240) do
+ActiveRecord::Schema.define(version: 2019_12_03_121650) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
@@ -27,21 +27,24 @@ ActiveRecord::Schema.define(version: 2019_11_12_000240) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "passed_tests", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.integer "test_id"
-    t.index ["test_id"], name: "index_passed_tests_on_test_id"
-    t.index ["user_id"], name: "index_passed_tests_on_user_id"
-  end
-
   create_table "questions", force: :cascade do |t|
     t.string "text", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "test_id"
     t.index ["test_id"], name: "index_questions_on_test_id"
+  end
+
+  create_table "test_passages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.integer "current_question_id"
+    t.integer "correct_questions", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["current_question_id"], name: "index_test_passages_on_current_question_id"
+    t.index ["test_id"], name: "index_test_passages_on_test_id"
+    t.index ["user_id"], name: "index_test_passages_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -64,9 +67,10 @@ ActiveRecord::Schema.define(version: 2019_11_12_000240) do
   end
 
   add_foreign_key "answers", "questions"
-  add_foreign_key "passed_tests", "tests"
-  add_foreign_key "passed_tests", "users"
   add_foreign_key "questions", "tests"
+  add_foreign_key "test_passages", "questions", column: "current_question_id"
+  add_foreign_key "test_passages", "tests"
+  add_foreign_key "test_passages", "users"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "users"
 end
