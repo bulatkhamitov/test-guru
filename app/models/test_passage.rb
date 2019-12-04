a5a5b79276e744b0ca106eb684f58fcd2914aa42
bulatkhamitov <bulat.khamitov@gmail.com>
2019-12-05 01:49:13 +0300

    Assign next question using callback

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,6 +4,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: "Question", optional: true
 
   before_validation :before_validation_find_first_question, on: :create
+  before_validation :before_validation_find_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -14,7 +15,6 @@ class TestPassage < ApplicationRecord
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
   end
 
@@ -22,6 +22,10 @@ class TestPassage < ApplicationRecord
 
   def before_validation_find_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_find_next_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
