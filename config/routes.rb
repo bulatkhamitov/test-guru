e94f044dfb259a828c2c083fdc93ed1836ab8439
bulatkhamitov <bulat.khamitov@gmail.com>
2019-12-09 10:58:43 +0300

    Add header & user logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get :login, to: 'sessions#new'
 
+  delete :logout, to: 'sessions#destroy'
+
   resources :users, only: :create
 
   resources :sessions, only: :create
