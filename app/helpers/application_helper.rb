ca4fc959797fad93cba65cf39c5008a56fb61db4
bulatkhamitov <bulat.khamitov@gmail.com>
2019-12-09 09:56:05 +0300

    Add flash messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,10 @@ module ApplicationHelper
   def thinknetica
     link_to "Онлайн-школа Thinknetica", "https://thinknetica.com/", target: :_blank
   end
+
+  def flash_message
+    flash.map do |key, message|
+      content_tag :p, message, class: "flash #{key}"
+    end.join.html_safe
+  end
 end
