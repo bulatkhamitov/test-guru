f2a51d18d5645df3cbe80932833a7ace06ff42ec
bulatkhamitov <bulat.khamitov@gmail.com>
2019-12-18 19:56:53 +0300

    Add redirection of admin to  admin/tests after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(user)
+    if user.is_a?(Admin)
+      admin_tests_path
+    else
+      tests_path
+    end
+  end
 end
