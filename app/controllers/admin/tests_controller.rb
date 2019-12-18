0335315cd226e4079e21aaff75d0bf6f521da33a
bulatkhamitov <bulat.khamitov@gmail.com>
2019-12-18 20:32:50 +0300

    Make admin designated author of new test

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -25,6 +25,7 @@ class Admin::TestsController < Admin::BaseController
 
   def create
     @test = Test.new(test_params)
+    @test.author = current_user
     if @test.save
       redirect_to admin_tests_path
     else
