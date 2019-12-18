49c82ae239a0b30bde7462f4953a3b7686732c66
bulatkhamitov <bulat.khamitov@gmail.com>
2019-12-18 19:52:15 +0300

    Update & correct links in accordance to admin namspace

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -17,7 +17,7 @@ class Admin::TestsController < Admin::BaseController
 
   def update
     if @test.update(test_params)
-      redirect_to @test
+      redirect_to admin_tests_path
     else
       render :edit
     end
@@ -26,7 +26,7 @@ class Admin::TestsController < Admin::BaseController
   def create
     @test = Test.new(test_params)
     if @test.save
-      redirect_to @test
+      redirect_to admin_tests_path
     else
       render :new
     end
@@ -34,7 +34,7 @@ class Admin::TestsController < Admin::BaseController
 
   def destroy
     @test.destroy
-    redirect_to tests_path
+    redirect_to admin_tests_path
   end
 
   def start
