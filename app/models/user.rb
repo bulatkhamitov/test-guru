e3bff6219224485d558204db7bc25b59b680ad07
bulatkhamitov <bulat.khamitov@gmail.com>
2019-12-19 18:38:27 +0300

    Remove email validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,6 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :authored_tests, class_name: 'Test', dependent: :destroy
 
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
   def passed_by_level(level)
     tests.where(level: level)
   end
