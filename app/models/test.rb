f9ad9ed85fd86da9800b2f434227114db054c933
bulatkhamitov <bulat.khamitov@gmail.com>
2020-01-12 03:01:59 +0300

    Add validation for time_limit attribute

--- app/models/test.rb
+++ app/models/test.rb
@@ -8,6 +8,7 @@ class Test < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: { scope: :level }
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :time_limit, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
