291e643c229ba547daa37baf8bb1dfd51977700f
bulatkhamitov <bulat.khamitov@gmail.com>
2020-01-14 01:18:51 +0300

    Update DB schema in accordance to Badge model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_160152) do
+ActiveRecord::Schema.define(version: 2020_01_13_162211) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2020_01_08_160152) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "question_id"
     t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "badges", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "icon_url", null: false
+    t.string "rule", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "categories", force: :cascade do |t|
