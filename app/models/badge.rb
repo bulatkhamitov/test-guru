0d3eca69252489a9e77e485735ca1dc7c3f5dac8
bulatkhamitov <bulat.khamitov@gmail.com>
2020-01-16 19:46:48 +0300

    Allow blank value for rule attribute

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,5 +2,6 @@ class Badge < ApplicationRecord
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
-  validates :title, :icon_url, :rule, presence: true
+  validates :title, :icon_url, presence: true
+  validates :rule, presence: true, allow_blank: true
 end
