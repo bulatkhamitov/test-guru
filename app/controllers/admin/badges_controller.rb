46608184608fad5bff2819ff682407fc5dbd9f85
bulatkhamitov <bulat.khamitov@gmail.com>
2020-01-16 20:25:08 +0300

    Fill Admin::BadgesController

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,2 +1,43 @@
 class Admin::BadgesController < Admin::BaseController
+  def index
+    @badges = Badge.all
+  end
+
+  def new
+    @badge = Badge.new
+  end
+
+  def edit; end
+
+  def update
+    if @badge.update(badge_params)
+      redirect_to admin_badges_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @badge = Badge.create(badge_params)
+    if @badge.save
+      redirect_to admin_badges_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @badge.destroy
+    redirect_to admin_badges_path
+  end
+
+  private
+
+  def find_badge
+    @badge = Badge.find(params[:id])
+  end
+
+  def badge_params
+   params.require(:badge).permit(:title, :icon_url, :rule)
+  end
 end
