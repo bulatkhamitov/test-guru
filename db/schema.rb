02173a261b6b378c6e12b82a368a64e1c91af069
bulatkhamitov <bulat.khamitov@gmail.com>
2020-01-19 16:56:30 +0300

    Add value attribute for Badge model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_17_210406) do
+ActiveRecord::Schema.define(version: 2020_01_19_134238) do
 
   create_table "answers", force: :cascade do |t|
     t.boolean "correct", default: false, null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_01_17_210406) do
     t.string "rule", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "value"
   end
 
   create_table "categories", force: :cascade do |t|
