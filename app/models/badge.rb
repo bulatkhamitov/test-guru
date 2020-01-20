9f86b1e8b1d8c8a97e7aec1c86bcf3bdd72a3a6a
bulatkhamitov <bulat.khamitov@gmail.com>
2020-01-21 00:28:56 +0300

    Edit validation for Badge model attributes

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,12 +2,11 @@ class Badge < ApplicationRecord
   RULES = %w[FirstTryBadge
              CategoryBadge
              LevelBadge]
-             
+
   has_many :user_badges, dependent: :destroy
   has_many :users, through: :user_badges
 
-  validates :title, :icon_url, presence: true
-  validates :rule, presence: true, allow_blank: true
+  validates :title, :icon_url, :rule, :rule_value, presence: true
 
   def self.icons
     Dir.entries('app/assets/images/badges').reject do |file|
