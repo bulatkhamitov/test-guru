d5cfb0ed95d27c933ea6ac51f026d4549c46e881
bulatkhamitov <bulat.khamitov@gmail.com>
2020-01-22 23:50:01 +0300

    Add validation for rule & rule_value

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -3,6 +3,8 @@ class Badge < ApplicationRecord
   has_many :users, through: :user_badges
 
   validates :title, :icon_url, :rule, :rule_value, presence: true
+  validates :rule, inclusion: { in: BadgeService::RULES, message: 'Wrong rule' }
+  validates :rule_value, uniqueness: { scope: :rule, message: 'The badge already exists' }
 
   def self.icons
     Dir.entries('app/assets/images/badges').reject do |file|
