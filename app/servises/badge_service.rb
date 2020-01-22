4959b4c80437ebba4453a7cbe949ff6a82e5efb3
bulatkhamitov <bulat.khamitov@gmail.com>
2020-01-22 03:13:57 +0300

    Extend BadgeService

--- app/servises/badge_service.rb
+++ app/servises/badge_service.rb
@@ -1,7 +1,25 @@
-class BadgesService
+class BadgeService
   def initialize(test_passage)
     @test_passage = test_passage
     @user = test_passage.user
     @test = test_passage.test
+  end
+
+  def call
+    Badge.all.select do |badge|
+      send(badge.rule.underscore.to_sym, badge)
+    end
+  end
+
+  def first_try_badge(blank)
+    @test_passage.passed? && @user.tests.where(id: @test.id).count == 1
+  end
+
+  def category_badge(category)
+    @test_passage.passed? && (Test.sort_by_category(category).count == @user.tests.where(category: category).uniq.count)
+  end
+
+  def level_badge(level)
+    @test_passage.passed? && (Test.all.where(level: level).count == @user.tests.where(level: level).uniq.count)
   end
 end
